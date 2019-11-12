ee6b5514b0eda76081e2334315ffff0e5f92975d
natallialemiasheuskaya <natkaber1995@gmail.com>
2019-11-12 16:39:38 +0300

    Initial Commit

---
+++ HomeWork1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork1.2
+//
+//  Created by Наташа Лемешевская on 11/12/19.
+//  Copyright © 2019 Наташа Лемешевская. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
