2f916da1ef848398c015ad32dc89dc44b2eacc62
natallialemiasheuskaya <natkaber1995@gmail.com>
2019-11-12 17:56:05 +0300

    homeWork1.2

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var greenColor: UIView!
+    @IBOutlet var buttonStart: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let radius = redColor.frame.width/2
+        redColor.alpha = 0.3
+        redColor.layer.cornerRadius = radius
+        yellowColor.alpha = 0.3
+        yellowColor.layer.cornerRadius = radius
+        greenColor.alpha = 0.3
+        greenColor.layer.cornerRadius = radius
+        
     }
 
+    @IBAction func startAction() {
+        if redColor.alpha == 1 {
+            yellowColor.alpha = 1
+            redColor.alpha = 0.3
+        }else if yellowColor.alpha == 1 {
+            greenColor.alpha = 1
+            yellowColor.alpha = 0.3
+        }else if greenColor.alpha == 1 {
+            redColor.alpha = 1
+            greenColor.alpha = 0.3
+        }else {
+            buttonStart.setTitle("NEXT", for: .normal)
+            redColor.alpha = 1
+        }
+    }
 
 }
 
